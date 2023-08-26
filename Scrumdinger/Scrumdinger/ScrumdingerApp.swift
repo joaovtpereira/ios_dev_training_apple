0272fa3af4df4c5926e4da784ca3236e1b9df6da
joaovtpereira <joaovitorpessoal2@gmail.com>
2023-08-25 23:39:51 -0300

    Class 1:
    Understand about of basic from building block of Swift.UI, and to apply the concept of views, stacks and modifiers.

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by João Vitor Pereira on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
